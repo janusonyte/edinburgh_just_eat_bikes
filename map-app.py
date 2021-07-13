43783afd935d5a61f3571776a955d0589e3d24eb
janusonyte <janusonytegabriele@gmail.com>
2021-07-13 15:25:54 +0100

    adding markdowns

--- map-app.py
+++ map-app.py
@@ -19,7 +19,8 @@ st.title("Bike Hire in Edinburgh")
 st.subheader(
     "Exploring bike hire trends, station availability and usage through socioeconomic lens")
 st.subheader(
-    "To select a different month or year, use the sliders on the left side")
+    "To select a different month or year, open the sidebar on the left and use the sliders.")
+st.markdown("This interactive webpage uses Just Eat bike hire data in Edinburgh. Two types of data sets were used 1) a general one, which shows _all_ stations in Edinburgh, 2) and a collection of monthly datasets from 2018 to 2021, which show the usage of bike hire stations and is used in the interactive map (further down in the page). ")
 
 
 @st.cache(persist=True, allow_output_mutation=True)
@@ -96,7 +97,8 @@ def generate_color_from_density(hire_density_df):
         normalised_density = (
             (row["density"] - min_density) / (max_density - min_density))
         # appending the list with converted HEX values for colors
-        colors.append(matplotlib.colors.rgb2hex(cm.hot(normalised_density)))
+        colors.append(matplotlib.colors.rgb2hex(
+            cm.gist_heat(normalised_density)))
     # create new column colors and assign the HEX values
     hire_density_df["color"] = colors
 
@@ -108,6 +110,8 @@ with open('station_information.json') as f:
 # build dataframe from station_dict
 stations_df = pd.DataFrame.from_dict(stations_dict["data"]["stations"])
 
+st.markdown(
+    "Below is a table which contains general data about all Just Eat bike hire stations in Edinburgh, dated July 2021.")
 stations_df
 
 
@@ -144,13 +148,14 @@ if os.path.isfile(filename):
 
     # plotting the table
     st.markdown(
-        "The table below is the amended table of the bike hire data. X axis has the station name, start station latitute and longitude, number of bikes that were rented from that station, and corresponding color code for density.")
+        "The table below is the amended table of the bike hire data above. X axis has the station name, start station latitute and longitude, number of bikes that were rented from that station, and corresponding color code for density. This is then used for the map (see below).")
     st.markdown(
         "Y axis has the station ID. The stations are sorted in a descending order by number of hires.")
     sorted_hire_density_df = hire_density_df.sort_values(
         by=['density'], ascending=False)
     sorted_hire_density_df
     # plotting the boundaries of the map
+
     folium.Choropleth(
         # geo_data = geometry data for boundaries
         geo_data=boundaries_and_depri_gdf,
@@ -162,10 +167,12 @@ if os.path.isfile(filename):
         # matches the 'geo_data' to the the 'data'
         # need to use the 'feature.properties' as it uses the GeoJSON format
         key_on="feature.properties.DataZone",
-        fill_color="YlGn",
+        fill_color="RdYlBu",
         fill_opacity=0.7,
         line_opacity=0.2,
-        legend_name="Deprivation",
+        line_color="Black",
+        bins=9,
+        legend_name="Level of Deprivation",
     ).add_to(m)
 
     # The colour data is applied to each marker.
@@ -189,16 +196,16 @@ if os.path.isfile(filename):
     # displaying the map
     st.markdown("The map below displays the total level of deprivation (lighter green is higher deprivation, darker is more affluent) from 1-10.")
     st.markdown("Deprivation scores include income domain, employment domain, health domain, education/skills domain, housing domain, geographic access domain, crime rank.")
+    st.markdown("The Scottish Index of Multiple Deprivation (2020) and its data were used to map out the levels of deprivation throughout Edinburgh. For more information about SIMD, head to this website: https://www.gov.scot/collections/scottish-index-of-multiple-deprivation-2020/?utm_source=redirect&utm_medium=shorturl&utm_campaign=simd ")
     st.markdown("The markers show where bikes were rented from (start stations) in a form of heat plotting. The lighter the colour of the marker (eg. white), the more a particular station was used. A colour closer to black would mean that the station was rarely used.")
     st.markdown("Clicking a marker will produce a pop up, which shows the name of the station and how many times a bike was rented from it.")
     folium_static(m)
     st.markdown(
-        "This data tells a two-part story: 1) where the bike hire stations are located 2) bike rental trends.")
+        "This data tells a two-part story: 1) where the bike hire stations are located 2) bike rental trends for the month and year that was selected.")
     st.markdown("It is natural, that the bike hire stations are located closer to the city centre, but a high number of stations are also located in areas of mid-to-high level of affluence.")
-    st.markdown("Most striking is the complete absence (or near complete)  of stations in Granton, Muirhouse, Craigmillar, Niddrie, and West of Edinburgh (Gorgie, Saughton, Slateford).")
-    st.markdown("Most stations seem to be concentrated just above and below the city centre, most of which often show up dark on the map, indicating lack of use.")
+    st.markdown("Most striking is the complete absence (or near complete)  of stations in Granton, Muirhouse, Craigmillar, Niddrie, and West of Edinburgh (Gorgie, Saughton, Slateford). A further investigation (below), will show that the level of deprivation of an area plays a big role in station placement throughout Edinburgh.")
 
-    st.markdown("There is also scope for looking at how bike hire station usage changed when the pandemic hit. For stations located in more touristy spots like Portobello or Crammond, did the number of hires drop? And if so, was there a higher uptake in bikes rented elsewhere?")
+    st.markdown("While it would be interesting to look at how bike hire trends were affected by the COVID-19 pandemic, this was not explored.")
 
     # adding number of stations in each area
     column_num_stations = {}
@@ -217,7 +224,7 @@ if os.path.isfile(filename):
             pass
     boundaries_and_depri_gdf["num_stations"] = column_num_stations.values()
 
-    boundaries_and_depri_gdf
+    # boundaries_and_depri_gdf
 
     # computing deprivation for each station
     column_depri = []
@@ -239,7 +246,7 @@ if os.path.isfile(filename):
     stations_df["deprivation"] = column_depri
 
     # removing the -1 deprivation areas
-    stations_df
+    # stations_df
     stations_df.loc[1, 'deprivation'] = 4
     stations_df.loc[2, 'deprivation'] = 5
     stations_df.loc[23, 'deprivation'] = 6
@@ -248,12 +255,15 @@ if os.path.isfile(filename):
     stations_df.loc[73, 'deprivation'] = 9
     stations_df.loc[79, 'deprivation'] = 5
 
-    #stations_df.set_value('deprivation', '1', '4')
+    # stations_df.set_value('deprivation', '1', '4')
+    st.markdown("")
+    st.markdown("Below is a table of all available Just Eat bike hire stations (general data). The last column, named 'deprivation' shows which level of deprivation the station is located in. Level 1 is the area with highest level of deprivation, whereas level 10 is the most 'affluent'.")
+
     stations_df
 
-    st.markdown(
-        "The table below shows the 'cleaned' data with the missing deprivation levels removed")
-    #stations_df_clean = stations_df[stations_df['deprivation'] != -1]
+    # st.markdown(
+    #     "The table below shows the 'cleaned' data with the missing deprivation levels removed")
+    # stations_df_clean = stations_df[stations_df['deprivation'] != -1]
 
     # stations_df_clean
 
@@ -266,7 +276,7 @@ if os.path.isfile(filename):
         else:
             depri_num_of_stations[depri] += row_locations["num_stations"]
 
-    st.markdown(depri_num_of_stations)
+    # st.markdown(depri_num_of_stations)
 
     # diction = {"1": 0, "2": 2, "3": 1, "4": 9, "5": 9,
     #            "6": 6, "7": 8, "8": 5, "9": 11, "10": 28}
@@ -280,15 +290,19 @@ if os.path.isfile(filename):
     dictdata_df.loc[4, 'number of stations'] = 10
     dictdata_df.loc[6, 'number of stations'] = 7
     dictdata_df.loc[9, 'number of stations'] = 10
-    dictdata_df
+    # dictdata_df
     dictdata_df.plot.bar(
         y='number of stations', rot=0)
-
     st.markdown(
-        "Bar graph showing the number of stations per each deprivation level")
+        "Below is a bar graph showing the number of stations per each deprivation level. _X_ axis shows the number of stations, and _Y_ axis is the level of deprivation from 1 to 10.")
     st.bar_chart(dictdata_df, height=400)
     dictdata_df.reset_index(inplace=True)
-    st.markdown(dictdata_df.corr(method='pearson', min_periods=1))
+    dictdata_df.rename(
+        columns={'index': 'level of deprivation'}, inplace=True)
+    st.markdown("A summary table of the number of stations and level of deprivation is available below. Clicking on column names will sort the table by ascending or descending order.")
+    dictdata_df
+    st.markdown("A correlation between the number of stations and deprivation level was conducted. Below is a correlation matrix. There is a strong relationship (0.7208) between number of stations and level of deprivation. While correlation does not equal causation, these findings still show that there is a relationship between deprivation and where stations are located in Edinburgh. More affluent areas have a higher number of stations, allowing the residents or visitors to those areas to have better access to green transport. ")
+    st.write(dictdata_df.corr(method='pearson', min_periods=1))
 
 
 else:
